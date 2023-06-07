3a82b4074e9c9d39285cdc3161f52144b51e9fb0
Robbie Harwood <rharwood@pm.me>
2023-06-07 19:22:58 -0400

    What if you could loop forever?
    Signed-off-by: Robbie Harwood <rharwood@pm.me>

--- dictle.py
+++ dictle.py
@@ -24,67 +24,73 @@ def get_in(prompt: str) -> str:
 def okay(word: str, length: int) -> bool:
     return len(word) == length and word.islower() and word.isalpha()
 
-p(Fore.LIGHTBLUE_EX, "Welcome to dictle!")
-dictionary = get_in("What dictionary are we using? [/usr/share/dict/words]: ")
-if len(dictionary) == 0:
-    dictionary = "/usr/share/dict/words"
-if not os.path.exists(dictionary):
-    print(f"{dictionary} not found; giving up")
-    exit(1)
+def run_one_dictle(word: str, length: int, words_set: set[str]) -> None:
+    while True:
+        line = sys.stdin.readline()
+        assert(line[-1] == "\n")
+        line = line[:-1]
 
-with open(dictionary, "r") as f:
-    words = f.read().split("\n")
+        # clear the entry
+        sys.stdout.write("\033[F\033[K")
+        sys.stdout.flush()
 
-length_raw = get_in("What length words would you like? [5]: ")
-if length_raw == "":
-    length_raw = "5"
-length = int(length_raw)
-assert(length > 0)
+        if len(line) != length:
+            p(Fore.RED, "Wrong length - try again")
+            continue
+        elif line == word:
+            p(Fore.LIGHTGREEN_EX, word)
+            p(Fore.LIGHTBLUE_EX, "Congratulations - you win!")
+            return
+        elif not okay(line, length):
+            p(Fore.RED, f"\"{line}\" contains invalid characters")
+            continue
+        elif line not in words_set:
+            p(Fore.RED, f"\"{line}\" is not in the dictionary")
+            continue
 
-words = [word for word in words if okay(word, length)]
-random.shuffle(words)
+        # Conduct two passes so that we flag the right number of missing
+        # letters.
+        missing: list[str] = []
+        z = list(zip(list(word), list(line))) # zip results drain
+        for wl, ll in z:
+            if ll != wl:
+                missing += wl
 
-words_set = set(words) # keep a copy of the wordlist around
-word = words.pop()
-p(Fore.LIGHTBLUE_EX, "Word selected.  Guess away!")
+        for wl, ll in z:
+            if wl == ll:
+                w(Back.GREEN, ll)
+            elif ll in missing:
+                missing.remove(ll)
+                # yellow typically is tinted toward orange
+                w(Back.YELLOW, ll)
+            else:
+                sys.stdout.write(ll)
+        print()
+    return
 
-while True:
-    line = sys.stdin.readline()
-    assert(line[-1] == "\n")
-    line = line[:-1]
+if __name__ == "__main__":
+    p(Fore.LIGHTBLUE_EX, "Welcome to dictle!")
+    dpath = get_in("What dictionary are we using? [/usr/share/dict/words]: ")
+    if len(dpath) == 0:
+        dpath = "/usr/share/dict/words"
+    if not os.path.exists(dpath):
+        print(f"{dpath} not found; giving up")
+        exit(1)
 
-    # clear the entry
-    sys.stdout.write("\033[F\033[K")
-    sys.stdout.flush()
-    
-    if len(line) != length:
-        p(Fore.RED, "Wrong length - try again")
-        continue
-    elif line == word:
-        p(Fore.LIGHTGREEN_EX, word)
-        p(Fore.LIGHTBLUE_EX, "Congratulations - you win!")
-        exit(0)
-    elif not okay(line, length):
-        p(Fore.RED, f"\"{line}\" contains invalid characters")
-        continue
-    elif line not in words_set:
-        p(Fore.RED, f"\"{line}\" is not in the dictionary")
-        continue
+    with open(dpath, "r") as f:
+        words = f.read().split("\n")
 
-    # Conduct two passes so that we flag the right number of missing letters.
-    missing: list[str] = []
-    z = list(zip(list(word), list(line))) # zip results drain
-    for wl, ll in z:
-        if ll != wl:
-            missing += wl
+    length_raw = get_in("What length words would you like? [5]: ")
+    if length_raw == "":
+        length_raw = "5"
+    length = int(length_raw)
+    assert(length > 0)
 
-    for wl, ll in z:
-        if wl == ll:
-            w(Back.GREEN, ll)
-        elif ll in missing:
-            missing.remove(ll)
-            # yellow typically is tinted toward orange
-            w(Back.YELLOW, ll)
-        else:
-            sys.stdout.write(ll)
-    print()
+    words = [word for word in words if okay(word, length)]
+    random.shuffle(words)
+
+    words_set = set(words) # keep a copy of the wordlist around
+    while True:
+        word = words.pop()
+        p(Fore.LIGHTBLUE_EX, "Word selected.  Guess away!")
+        run_one_dictle(word, length, words_set)
